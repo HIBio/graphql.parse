59b2c2ae003a773d8d4245f77c71d2cd7d703402
Jonathan Carroll <github@jcarroll.com.au>
2022-10-04 08:44:44 +1030

    courier font for viewer preview (h/t @david-hibio)

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 clean_html <- function(qs) {
   qs <- gsub("\n", "<br>", qs)
   qs <- gsub(" ", "&nbsp;", qs)
-  htmltools::html_print(htmltools::HTML(qs))
+  htmltools::html_print(htmltools::div(htmltools::HTML(qs), style = "font-family:courier"))
 }
